34818835cacc178d8e6f4644f63346b8b1ae9203
SoftwareDevelopHER <kenyatta.martin1992@gmail.com>
2022-06-17 10:49:12 -0400

    Stand Up Fri 17th, 22

---
+++ Week9Day5.js
@@ -0,0 +1,12 @@
+June 17th, 2022
+
+Jacky : improve mock interview and self study; Excited about presenting 
+
+Romane: Happy to present
+
+Riya: Excited for presentation today; Working on interview with Charles; Improve on everything we learned
+
+SoYoung: Happy she found more info about WebStation; Need improvement on speaking skills and talking to others; Working on studying for interview;
+
+
+Kenyatta: Happy to present today; Working on speech delivery for presentation; Need improvement on technical terms
